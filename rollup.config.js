5b17ab3affd384cba52b6f440ebc41edee63268c
John Paul Ada <johnpaulada@Johns-Mac-mini.local>
2018-06-27 17:46:46 +0800

    Multiplatform deployment

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,23 @@
 import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify-es';
 
 export default {
   input: 'Maybe.js',
-  output: {
-    file: 'index.js',
-    format: 'umd',
-    name: 'Maybe'
-  },
+  output: [
+    {
+      file: 'index.js',
+      format: 'cjs'
+    },
+    {
+      file: 'index.es.js',
+      format: 'es'
+    },
+    {
+      file: 'index.min.js',
+      format: 'iife',
+      name: 'Maybe'
+    }
+  ],
   plugins: [
     babel({
       exclude: 'node_modules/**'
