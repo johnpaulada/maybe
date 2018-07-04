96505b5ded8c1a1fcd30e4cf2d4cef57d701b038
John Paul Ada <johnpaulada@Johns-Mac-mini.local>
2018-07-04 09:36:08 +0800

    Added branch to Just and Nothing

--- Just.js
+++ Just.js
@@ -4,7 +4,8 @@ const Just = x => ({
   inspect: f => console.log(`Just(${f(x)})`) || Just(f(x)),
   map: f => Just(f(x)),
   ap: fa => Just(fa.reduce(a => a)(x)),
-  chain: f => Just(f(x).reduce(x => x))
+  chain: f => Just(f(x).reduce(x => x)),
+  branch: (_, right) => Just(right(x))
 })
 
 export default Just
--- Nothing.js
+++ Nothing.js
@@ -1,10 +1,13 @@
+import Just from './Just'
+
 const Nothing = () => ({
   value: v => v,
   reduce: _ => null,
   inspect: _ => console.log(`Nothing`) || Nothing(),
   map: _ => Nothing(), 
   ap: _ => Nothing(),
-  chain: _ => Nothing()
+  chain: _ => Nothing(),
+  branch: (left, _) => Just(left())
 })
 
 export default Nothing
